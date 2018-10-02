24ee91a6a7c5f9065e9c0aa10fa07e5628bd21c8
Dmitry <crismsfiva@gmail.com>
2018-10-02 18:19:45 +0300

    Добавляет модуль загрузки своего фото

---
+++ js/upload-photo.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var preview = document.querySelector('.img-upload__preview img');
+  var fileChooser = document.querySelector('#upload-file');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
